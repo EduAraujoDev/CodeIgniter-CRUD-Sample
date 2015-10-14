b926fb4f7f528e25e077cfa85cce73b947c5918e
Eduardo Araujo <eduardo.araujo0@outlook.com>
2015-10-13 23:51:16 -0300

    Segundo Commit
    Incluido o Script do Banco de dados e ajustado o mesmo no arquivo de
    configuração no database.php

---
+++ Empresa.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE EMPRESA;
+
+CREATE TABLE empresa.departamento (
+id_departamento INTEGER NOT NULL PRIMARY KEY,
+nome VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE empresa.cargo (
+id_cargo INTEGER NOT NULL PRIMARY KEY,
+nome VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE empresa.funcionario (
+id_funcionario INTEGER NOT NULL PRIMARY KEY,
+nome VARCHAR(80) NOT NULL,
+dt_contratacao DATE NOT NULL,
+id_cargo INTEGER NOT NULL,
+  FOREIGN KEY (id_cargo) REFERENCES empresa.cargo (id_cargo) ON DELETE CASCADE,
+id_departamento INTEGER NOT NULL,
+  FOREIGN KEY (id_departamento) REFERENCES empresa.departamento (id_departamento) ON DELETE CASCADE
+);
